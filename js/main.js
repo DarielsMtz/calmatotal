3d012a183d532dd292bbf4288a55aa2fa0b2e9a1
DarielsMtz <darielsmartinez926@gmail.com>
2024-01-11 22:34:03 +0100

    Creacion de paguina de inicio y cartas de opciones

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const enlaces = document.querySelectorAll(".enlace");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const href = this.getAttribute("href");
+
+      // Aplicar transición de opacidad al salir de la página actual
+      document.querySelector(".contenido").style.opacity = 0;
+
+      // Esperar a que termine la animación antes de redirigir
+      setTimeout(function () {
+        window.location.href = href;
+      }, 500); // Ajusta el tiempo de espera según la duración de la transición
+    });
+  });
+});
