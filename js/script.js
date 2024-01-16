69df03e272ed3d0c98f9cd7e831b69674e5fb08b
DarielsMtz <darielsmartinez926@gmail.com>
2024-01-16 17:14:49 +0100

    Implementación de la logica

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// -------------------------------------------------------------------
 // Establecer un retraso de 5 segundos antes de iniciar la cuenta regresiva
 setTimeout(function () {
   // Establecer el tiempo inicial en segundos
@@ -20,12 +21,6 @@ setTimeout(function () {
 
     // Disminuir el tiempo restante
     tiempoRestante--;
-
-    // Detener la cuenta regresiva cuando el tiempo llega a cero
-    if (tiempoRestante < 0) {
-      clearInterval(intervalo);
-      document.getElementById("time").textContent = "Tiempo agotado";
-    }
   }
 
   // Llamar a la función inicialmente para mostrar el tiempo inicial
@@ -33,4 +28,55 @@ setTimeout(function () {
 
   // Establecer un intervalo para llamar a la función cada segundo
   const intervalo = setInterval(actualizarTiempo, 1000);
-}, 5000); // 5000 milisegundos = 5 segundos
+}, 3000);
+
+// -----------------------------------------------------------------------
+// Cuenta atras de 3seg
+setTimeout(function () {
+  // Establecer el tiempo inicial en segundos
+  let tiempoRestante = 3; // 3 segundos
+
+  // Función para actualizar el tiempo en el elemento div
+  function actualizarTiempo() {
+    // Actualizar el contenido del elemento div
+    document.getElementById("cuenta_atras").textContent = tiempoRestante;
+
+    // Disminuir el tiempo restante
+    tiempoRestante--;
+
+    // Detener la cuenta regresiva cuando el tiempo llega a cero
+    if (tiempoRestante < 0) {
+      clearInterval(intervalo);
+      document.getElementById("cuenta_atras").textContent = " ";
+    }
+  }
+  // Llamar a la función inicialmente para mostrar el tiempo inicial
+  actualizarTiempo();
+
+  // Establecer un intervalo para llamar a la función cada segundo
+  const intervalo = setInterval(actualizarTiempo, 1000);
+}, 1000);
+
+// ------------------------------------------------------------
+// Evento para detener todo
+document
+  .getElementById("stopButton")
+  .addEventListener("click", function (event) {
+    // Evitar el comportamiento predeterminado del clic
+    event.preventDefault();
+
+    // Obtener todas las animaciones en la página
+    const animaciones = document.getAnimations();
+    const time = document.getAttribute("time");
+
+    // Detener cada animación
+    animaciones.forEach(function (animacion) {
+      animacion.pause();
+    });
+    // Detener el intervalo
+    time.forEach(function (time) {
+      time.pause();
+    });
+
+    setInterval((window.location.href = "ejercicio01.html"), 2000);
+  });
