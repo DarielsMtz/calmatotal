a73a75e973b9c6d3465b847b0ade37225a191176
DarielsMtz <darielsmartinez926@gmail.com>
2024-01-19 16:32:57 +0100

    implementación de las demas opciones

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Establecer un retraso de 5 segundos antes de iniciar la cuenta regresiva
 setTimeout(function () {
   // Establecer el tiempo inicial en segundos
-  let tiempoRestante = 5 * 60; // 5 minutos en segundos
+  let tiempoRestante = 5 * 60;
 
   // Función para actualizar el tiempo en el elemento h1
   function actualizarTiempo() {
@@ -21,6 +21,82 @@ setTimeout(function () {
 
     // Disminuir el tiempo restante
     tiempoRestante--;
+  }
+
+  // Llamar a la función inicialmente para mostrar el tiempo inicial
+  actualizarTiempo();
+
+  // Establecer un intervalo para llamar a la función cada segundo
+  setInterval(actualizarTiempo, 1000);
+}, 3000);
+
+// -----------------------------------------------------------------------
+// Establecer un retraso de 3 segundos antes de iniciar la cuenta regresiva
+// 10min de tiempo
+setTimeout(function () {
+  // Establecer el tiempo inicial en segundos
+  let tiempoRestante = 10 * 60;
+
+  // Función para actualizar el tiempo en el elemento h1
+  function actualizarTiempo() {
+    // Calcular minutos y segundos restantes
+    const minutos = Math.floor(tiempoRestante / 60);
+    const segundos = tiempoRestante % 60;
+
+    // Formatear los minutos y segundos con ceros a la izquierda si es necesario
+    const minutosFormateados = minutos < 10 ? "0" + minutos : minutos;
+    const segundosFormateados = segundos < 10 ? "0" + segundos : segundos;
+
+    // Actualizar el contenido del elemento h1
+    document.getElementById(
+      "time02"
+    ).textContent = `${minutosFormateados}:${segundosFormateados} min`;
+
+    // Disminuir el tiempo restante
+    tiempoRestante--;
+
+    // Detener la cuenta regresiva cuando el tiempo llegue a cero
+    if (tiempoRestante < 0) {
+      clearInterval(intervalo);
+    }
+  }
+
+  // Llamar a la función inicialmente para mostrar el tiempo inicial
+  actualizarTiempo();
+
+  // Establecer un intervalo para llamar a la función cada segundo
+  const intervalo = setInterval(actualizarTiempo, 1000);
+}, 3000);
+
+// -----------------------------------------------------------------------
+// Establecer un retraso de 3 segundos antes de iniciar la cuenta regresiva
+// 15 min de tiempo
+setTimeout(function () {
+  // Establecer el tiempo inicial en segundos
+  let tiempoRestante = 15 * 60;
+
+  // Función para actualizar el tiempo en el elemento h1
+  function actualizarTiempo() {
+    // Calcular minutos y segundos restantes
+    const minutos = Math.floor(tiempoRestante / 60);
+    const segundos = tiempoRestante % 60;
+
+    // Formatear los minutos y segundos con ceros a la izquierda si es necesario
+    const minutosFormateados = minutos < 10 ? "0" + minutos : minutos;
+    const segundosFormateados = segundos < 10 ? "0" + segundos : segundos;
+
+    // Actualizar el contenido del elemento h1
+    document.getElementById(
+      "time03"
+    ).textContent = `${minutosFormateados}:${segundosFormateados} min`;
+
+    // Disminuir el tiempo restante
+    tiempoRestante--;
+
+    // Detener la cuenta regresiva cuando el tiempo llegue a cero
+    if (tiempoRestante < 0) {
+      clearInterval(intervalo);
+    }
   }
 
   // Llamar a la función inicialmente para mostrar el tiempo inicial
@@ -73,5 +149,5 @@ document
       animacion.pause();
     });
 
-    window.location.href = "ejercicio01.html";
+    window.location.href = "../inicio.html";
   });
